43210447aa98276e29dc7d3b68b6c88467f75352
Roberto Vega <droz.rv@gmail.com>
2025-02-02 08:31:42 -0500

    fixed infinite loop error and limited movie list to 6 movies at a time

--- app.js
+++ app.js
@@ -17,24 +17,6 @@ searchIcon.addEventListener('click', (event) => {
     }
 });
 
-searchInput.addEventListener('keypress', (event) => {
-    if (event.key === "Enter") {
-        const searchTerm = searchInput.value.toLowerCase();
-        
-        // Filter and limit to 6 movies
-        const filteredMovies = allMovies
-            .filter(movie => movie.Title.toLowerCase().includes(searchTerm))
-            .slice(0, 6);
-            
-        if (filteredMovies === searchTerm) {
-            displayMovies(filteredMovies.slice(0, 6));
-        } else {
-            console.log("No matching movies found");
-            displayMovies(allMovies.slice(0, 6));
-        }
-    }
-});
-
 async function fetchMovies(query) {
     const response = await fetch(`https://www.omdbapi.com/?apikey=34328269&s=${query}`);
     const data = await response.json();
@@ -48,12 +30,33 @@ async function renderMovies() {
     // const avengers = await fetchMovies("avengers");
     // const spiderman = await fetchMovies("spiderman");
     // const minions = await fetchMovies("minions");
-    // const nope = await fetchMovies("nope");``
+    // const nope = await fetchMovies("nope");
+    // allMovies = [...guardians, ...avengers, ...spiderman, ...minions, ...nope].slice(0, 6);
+
     
-    const movies = await fetchMovies("guardians", "avengers", "spiderman", "minions", "nope");
+    const movies = await fetchMovies("avengers");
+    const sixMovies = movies.slice(0, 6);
+    console.log(sixMovies);
     const moviesList = document.querySelector('.movie-selection-wrapper');
 
-    const moviesHTML = movies.map((movie) => {
+
+    searchInput.addEventListener('keypress', (event) => {
+        if (event.key === "Enter") {
+            const searchTerm = searchInput.value.toLowerCase();
+            
+                
+            if (sixMovies.includes(searchTerm)) {
+                
+            } else {
+                console.log("No matching movies found");
+                displayMovies(allMovies.slice(0, 6));
+            }
+        }
+    });
+
+
+
+    const moviesHTML = sixMovies.map((movie) => {
         return `<div class="movie-box">
                 <figure class="movie-image-wrapper">
                     <img class="movie-image" src="${movie.Poster}" alt="">
@@ -78,138 +81,10 @@ async function renderMovies() {
                 <div class="movie-name-wrapper">
                     <h4 class="movie-name">${movie.Title}</h4>
                 </div>
-            </div>
-            <div class="movie-box">
-                <figure class="movie-image-wrapper">
-                    <img class="movie-image" src="${movie.Poster}" alt="">
-                    <div class="movie-box-links">
-                        <div class="info-container">
-                            <h4 class="hover-title">${movie.Title}</h4>
-                            <div class="time info">
-                                <i class="fa-solid fa-clock icon"></i>
-                                <h4 class="right-side-text">136m</h4>
-                            </div>
-                            <div class="rating info">
-                                <i class="fa-solid fa-star icon"></i>
-                                <h4 class="right-side-rating">4.5</h4>
-                            </div>
-                            <div class="language info">
-                                <i class="fa-solid fa-earth-americas icon"></i>
-                                <h4 class="right-side-language">English</h4>
-                            </div>
-                        </div>
-                    </div>
-                </figure>
-                <div class="movie-name-wrapper">
-                    <h4 class="movie-name">${movie.Title}</h4>
-                </div>
-            </div>
-            <div class="movie-box">
-                <figure class="movie-image-wrapper">
-                    <img class="movie-image" src="${movie.Poster}" alt="">
-                    <div class="movie-box-links">
-                        <div class="info-container">
-                            <h4 class="hover-title">${movie.Title}</h4>
-                            <div class="time info">
-                                <i class="fa-solid fa-clock icon"></i>
-                                <h4 class="right-side-text">136m</h4>
-                            </div>
-                            <div class="rating info">
-                                <i class="fa-solid fa-star icon"></i>
-                                <h4 class="right-side-rating">4.5</h4>
-                            </div>
-                            <div class="language info">
-                                <i class="fa-solid fa-earth-americas icon"></i>
-                                <h4 class="right-side-language">English</h4>
-                            </div>
-                        </div>
-                    </div>
-                </figure>
-                <div class="movie-name-wrapper">
-                    <h4 class="movie-name">${movie.Title}</h4>
-                </div>
-            </div>
-            <div class="movie-box">
-                <figure class="movie-image-wrapper">
-                    <img class="movie-image" src="${movie.Poster}" alt="">
-                    <div class="movie-box-links">
-                        <div class="info-container">
-                            <h4 class="hover-title">${movie.Title}</h4>
-                            <div class="time info">
-                                <i class="fa-solid fa-clock icon"></i>
-                                <h4 class="right-side-text">136m</h4>
-                            </div>
-                            <div class="rating info">
-                                <i class="fa-solid fa-star icon"></i>
-                                <h4 class="right-side-rating">4.5</h4>
-                            </div>
-                            <div class="language info">
-                                <i class="fa-solid fa-earth-americas icon"></i>
-                                <h4 class="right-side-language">English</h4>
-                            </div>
-                        </div>
-                    </div>
-                </figure>
-                <div class="movie-name-wrapper">
-                    <h4 class="movie-name">${movie.Title}</h4>
-                </div>
-            </div>
-            <div class="movie-box">
-                <figure class="movie-image-wrapper">
-                    <img class="movie-image" src="${movie.Title}" alt="">
-                    <div class="movie-box-links ">
-                        <div class="info-container verse-info-links">
-                            <h4 class="hover-title verse-title">${movie.Title}</h4>
-                            <div class="time info">
-                                <i class="fa-solid fa-clock icon"></i>
-                                <h4 class="right-side-text">136m</h4>
-                            </div>
-                            <div class="rating info">
-                                <i class="fa-solid fa-star icon"></i>
-                                <h4 class="right-side-rating">4.5</h4>
-                            </div>
-                            <div class="language info">
-                                <i class="fa-solid fa-earth-americas icon"></i>
-                                <h4 class="right-side-language">English</h4>
-                            </div>
-                        </div>
-                    </div>
-                </figure>
-                <div class="movie-name-wrapper">
-                    <h4 class="movie-name">${movie.Title}</h4>
-                </div>
-            </div>
-            <div class="movie-box">
-                <figure class="movie-image-wrapper">
-                    <img class="movie-image" src="${movie.Poster}" alt="">
-                    <div class="movie-box-links">
-                        <div class="info-container nope-info-links">
-                            <h4 class="hover-title nope-title">${movie.Title}</h4>
-                            <div class="time info">
-                                <i class="fa-solid fa-clock icon"></i>
-                                <h4 class="right-side-text">136m</h4>
-                            </div>
-                            <div class="rating info">
-                                <i class="fa-solid fa-star icon"></i>
-                                <h4 class="right-side-rating">4.5</h4>
-                            </div>
-                            <div class="language info">
-                                <i class="fa-solid fa-earth-americas icon"></i>
-                                <h4 class="right-side-language">English</h4>
-                            </div>
-                        </div>
-                    </div>
-                </figure>
-                <div class="movie-name-wrapper">
-                    <h4 cla ss="movie-name">${movie.Title}</h4>
-                </div>
-            </div>`
-    }).join("");
-    
-    // allMovies = [...guardians, ...avengers, ...spiderman, ...minions, ...nope].slice(0, 6);
+            </div>`}).join("");
+            
     moviesList.innerHTML = moviesHTML
-
-    
+            
 }
 
 console.log('This is the render movie function: ' + renderMovies());
